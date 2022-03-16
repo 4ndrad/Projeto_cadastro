d432d6377c7b8ab45339f88c308b069e42808f59
Natan Andrade de Lima <natanal@primeit.com.br>
2022-03-16 14:53:29 -0300

    privei admin

--- cadastro/src/routes.js
+++ cadastro/src/routes.js
@@ -52,9 +52,6 @@ export const routes = [
         path: '/admin',
         name: 'admin',
         component: Admin,
-        meta: {
-            publica: true
-        }
         //privado
     },
 
